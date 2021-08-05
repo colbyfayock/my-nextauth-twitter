8f3d54d1210ab5949125e381c12fcb9a30ef39d8
colbyfayock <fayock@gmail.com>
2021-08-05 12:03:17 -0400

    Step 3: making an authenticated search request to the Twitter API

--- pages/api/twitter/search.js
+++ pages/api/twitter/search.js
@@ -1,17 +1,32 @@
+import Twitter from 'twitter-lite';
 import { getSession } from 'next-auth/client'
 import { getToken } from 'next-auth/jwt';
 
 export default async (req, res) => {
+  const body = JSON.parse(req.body);
+  const { query } = body;
+
   const session = await getSession({ req });
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET
   });
 
+  const client = new Twitter({
+    subdomain: 'api',
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: token.twitter.accessToken,
+    access_token_secret: token.twitter.refreshToken
+  });
+
   try {
+    const results = await client.get('search/tweets', {
+      q: query
+    });
     return res.status(200).json({
       status: 'Ok',
-      data: []
+      data: results.statuses
     });
   } catch(e) {
     return res.status(400).json({
