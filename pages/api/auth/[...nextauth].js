42715af39519c7b73c6a138cf45bbf7463e1de85
colbyfayock <fayock@gmail.com>
2021-08-04 22:50:45 -0400

    Step 1: making the Access Token and Refresh Token available on the token object

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,5 +7,22 @@ export default NextAuth({
       clientId: process.env.TWITTER_CONSUMER_KEY,
       clientSecret: process.env.TWITTER_CONSUMER_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async jwt(token, user, account = {}, profile, isNewUser) {
+      if ( account.provider && !token[account.provider] ) {
+        token[account.provider] = {};
+      }
+
+      if ( account.accessToken ) {
+        token[account.provider].accessToken = account.accessToken;
+      }
+
+      if ( account.refreshToken ) {
+        token[account.provider].refreshToken = account.refreshToken;
+      }
+
+      return token;
+    },
+  }
 });
